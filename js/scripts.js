d74609ffa3f69aca7d2dd2d9f8ba8cf6c32c795b
Aurora Shido-Wagenet and Randee Layosa <randee@>
2019-01-17 15:39:11 -0800

    add js if statemetn not working

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+$(function(){
+  $("#submitButton").click(function(event){
+    var aInput = $("#a").val());
+    var bInput = $("#b").val();
+    var cInput = $("#c").val();
+
+    var aParse = parseInt(aInput);
+    var bParse = parseInt(bInput);
+    var cParse = parseInt(cInput);
+
+    alert(heyy);
+
+    if ((aParse + bParse <= cParse || aParse + cParse <= bParse || bParse + cParse <= aParse) || aParse === 0 || bParse === 0 || cParse === 0) {
+      ("#not").show();
+    } else if (aParse === bParse && bParse === cParse && cParse === aParse) {
+      ("#equal").show();
+    } else if (aParse !== bParse && bParse !== cParse && cParse !== aParse) {
+      ("#scalene").show();
+    } else if (aParse === bParse || bParse === cParse || cParse === aParse) {
+      ("#isosceles").show();
+    }
+
+    event.preventDefault();
+  });
+});
